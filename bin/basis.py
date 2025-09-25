09d73a23d9d7ab5ed8650648df650e916682d3cc
Friedrich Neumann <friedrich.neumann@student.tu-freiberg.de>
2025-09-25 10:26:41 +0200

    change structure

---
+++ bin/basis.py
@@ -0,0 +1,14 @@
+import numpy as np
+from numpy.polynomial.chebyshev import Chebyshev
+
+def ChebExpCos(n, r, rcut, rcut_in, dcut_in, lam):
+
+    #Calculates the chebyshev basis functions
+    x =  1 - (2 * ((np.exp(-lam * (r / rcut)) - np.exp(-lam)) / (1 - np.exp(-lam))))
+    if n == 0:
+        Tn = 1
+        cheb_val = Tn
+    else :
+        Tn = Chebyshev.basis(n)
+        cheb_val = 0.5 - (0.5 * Tn(x))
+    return cheb_val
